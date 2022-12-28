3a26eaa222e5398eed85597ffbd64ba4540cdeff
Your Name <vuthinu28>
2022-12-28 13:26:33 +0700

    up js

---
+++ js/bao24h-jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $("#btn-open").click(function(){
+
+        $("#id01").css({"display":"block"});
+    })
+    $("#btn-close").click(function(){
+
+        $("#id01").css({"display":"none"});
+    })
+    $("#btn-cance").click(function(){
+
+        $("#id01").css({"display":"none"});
+    })
+   // end form-login
+});
